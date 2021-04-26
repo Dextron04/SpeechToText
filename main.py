090648bb14f523f08fdd5405b110b56857c637fd
Tushin4123 <61046945+Tushin4123@users.noreply.github.com>
2021-04-26 22:01:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from tkinter import *
+import pyttsx3
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+
+def SpeakText(command):
+    engine = pyttsx3.init()
+    engine.setProperty('volume', 10)
+    engine.setProperty('rate', 130)
+    engine.say(command)
+    engine.runAndWait()
+
+while(1):
+
+    try:
+
+        with sr.Microphone() as source2:
+
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+
+            audio2 = r.listen(source2)
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+            print("Did you say " + MyText)
+            SpeakText(MyText)
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+    except sr.UnknownValueError:
+        print("Unknown error occured")
